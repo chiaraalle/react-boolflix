91b6a3e09aa291af20aff695f7233997cdcc04f6
chiaralle <chiaraallegrino@gmail.com>
2025-03-07 20:48:56 +0100

    add:stars

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,6 +2,21 @@ import { useGlobalContext } from "../context/globalContext";
 
 function Main() {
     const {movies, series} = useGlobalContext();
+
+    const handleStar= (vote) => {
+        const stars = [];
+        for (let i = 0; i < 5; i++) {
+            if (i < vote / 2) {
+                stars.push(<i className="fas fa-star"></i>)
+            } else {
+                stars.push(<i className="far fa-star"></i>)
+            }
+        }
+        return stars;
+    }
+
+
+
     return(
         <main>
             <h3>Film</h3>
